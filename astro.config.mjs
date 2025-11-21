7fe7c7e80e6233ffca6aec4675b200520e5a6ea6
mikael.linhares <mikael.linhares@techbigdata.io>
2025-11-20 23:37:35 -0300

    feat:att

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import tailwindcss from '@tailwindcss/vite';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://mikaellinhares.github.io',
-  base: '/',
+  base: '/MLTech',
   output: 'static',
   vite: {
     plugins: [tailwindcss()]
